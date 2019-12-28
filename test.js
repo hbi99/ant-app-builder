2949020c40e37ae9ae076611dcc363370a4a940e
Hakan Bilgin <hbi@longscript.com>
2019-12-28 01:17:17 +0100

    enabling slightly higher script uglifyer

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const { Build } = require("./lib/")
 
 let srcDir = path.join(__dirname, "temp/finder")
 let destDir = path.join(__dirname, "temp/_build")
-let uglify = false
+let uglify = true
 
 let runIt = async() => {
 	let b = await Build(srcDir, destDir, uglify)
