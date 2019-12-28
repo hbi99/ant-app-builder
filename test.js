0ab2904d41551389ad17d763fcc769fa4e7d8623
Hakan Bilgin <hbi@longscript.com>
2019-12-28 02:15:03 +0100

    better summary name for file list

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 const path = require("path")
 const { Build } = require("./lib/")
 
-let srcDir = path.join(__dirname, "temp/finder")
+let srcDir = path.join(__dirname, "temp/test")
 let destDir = path.join(__dirname, "temp/_build")
 let uglify = true
 
