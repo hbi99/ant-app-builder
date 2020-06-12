c7d2d06b0dc1669c8aa89b47b6a941e206552f03
Hakan Bilgin <hbi99@hotmail.com>
2020-06-12 12:49:52 +0200

    bugfix

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 const path = require("path")
 const { Build } = require("./lib/")
 
-let srcDir = path.join(__dirname, "temp/preferences")
+let srcDir = path.join(__dirname, "temp/photoshop")
 let destDir = path.join(__dirname, "temp/_build")
 let uglify = false
 
