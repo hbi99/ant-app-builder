ea33374ac16ee2f2e7ef5b89e6e62cafb3cfb5f2
Hakan Bilgin <hbi99@hotmail.com>
2020-09-13 01:11:23 +0200

    adjusting terser minify options

--- test.js
+++ test.js
@@ -7,13 +7,11 @@ let destDir = path.join(__dirname, "temp/_build")
 let uglify = false
 
 let runIt = async() => {
+	let src = path.join(srcDir, "/src/js/modules")
+	await MinifyModules(src)
+	
 	let b = await Build(srcDir, destDir, uglify)
 	//console.log(b)
 }
-//runIt()
+runIt()
 
-let modules = async() => {
-	let src = path.join(srcDir, "/src/js/modules")
-	await MinifyModules(src)
-}
-modules()
