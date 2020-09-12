c4b6b5ed4746b3f3753043111cde5e87983c1822
Hakan Bilgin <hbi99@hotmail.com>
2020-09-12 16:42:29 +0200

    added module minify

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 
 const path = require("path")
-const { Build } = require("./lib/")
+const { Build, MinifyModules} = require("./lib/")
 
-let srcDir = path.join(__dirname, "temp/vermeer")
+let srcDir = path.join(__dirname, "temp/arcad")
 let destDir = path.join(__dirname, "temp/_build")
 let uglify = false
 
@@ -10,5 +10,10 @@ let runIt = async() => {
 	let b = await Build(srcDir, destDir, uglify)
 	//console.log(b)
 }
-
 runIt()
+
+let modules = async() => {
+	let src = path.join(srcDir, "/src/js/modules")
+	await MinifyModules(src)
+}
+//modules()
