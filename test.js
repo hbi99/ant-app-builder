6ea367a7e8bf38ac6c6e66be0099a32f80578295
Hakan Bilgin <hbi99@hotmail.com>
2020-09-14 01:24:03 +0200

    remove minMods, added bundle method

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 
 const path = require("path")
-const { Build, MinifyModules} = require("./lib/")
+const { Build, Bundle} = require("./lib/")
 
 let srcDir = path.join(__dirname, "temp/arcad")
 let destDir = path.join(__dirname, "temp/_build")
 let uglify = false
 
 let runIt = async() => {
-	// let src = path.join(srcDir, "/src/js/modules")
-	// await MinifyModules(src)
+	let src = path.join(srcDir, "/src/js/bundle.js")
+	await Bundle(src)
 	
-	let b = await Build(srcDir, destDir, uglify)
+	//let b = await Build(srcDir, destDir, uglify)
 	//console.log(b)
 }
 runIt()
