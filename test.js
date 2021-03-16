f5c22a14f49c7e1a9802467a2703a5b779aa8f75
Hakan Bilgin <hbi99@hotmail.com>
2021-03-16 19:11:25 +0100

    added runRequires to build result

--- test.js
+++ test.js
@@ -2,14 +2,17 @@
 const path = require("path")
 const { Build } = require("./lib/")
 
-let source = path.join(__dirname, "temp/solitaire")
+let source = path.join(__dirname, "temp/spotify")
 let destination = path.join(__dirname, "temp/_build")
 let uglify = false
 let compress = true
 
 let runIt = async () => {
 	Build({ source, destination, uglify, compress })
-		.then(success => console.log("Done!"))
+		.then(build => {
+			console.log(build)
+			console.log("Done!")
+		})
 		.catch(e => console.log(e))
 }
 runIt()
