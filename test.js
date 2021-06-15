0c97f9db180167e1e14cb70277061b0aba7bc5d6
Hakan Bilgin <hbi99@hotmail.com>
2021-06-15 18:10:58 +0200

    prevents script from failure on "]]>"

--- test.js
+++ test.js
@@ -2,10 +2,10 @@
 const path = require("path")
 const { Build } = require("./lib/")
 
-let source = path.join(__dirname, "temp/spotify")
+let source = path.join(__dirname, "temp/eniac")
 let destination = path.join(__dirname, "temp/_build")
 let uglify = false
-let compress = true
+let compress = false
 
 let runIt = async () => {
 	Build({ source, destination, uglify, compress })
